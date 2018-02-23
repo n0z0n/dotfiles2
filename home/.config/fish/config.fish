172a5fba3475a160210aaadfdd10a3c4d988031f
Nozomi MIURA <mail+github@n0z.me>
2018-02-24 00:10:27 +0900

    Initial commit

---
+++ home/.config/fish/config.fish
@@ -0,0 +1,6 @@
+umask 0022
+
+set -x PATH     /home/linuxbrew/.linuxbrew/bin $PATH
+set -x PATH     /home/linuxbrew/.linuxbrew/sbin $PATH
+set -x MANPATH  /home/linuxbrew/.linuxbrew/share/man $MANPATH
+set -x INFOPATH /home/linuxbrew/.linuxbrew/share/info $INFOPATH
